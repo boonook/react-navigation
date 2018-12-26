cb3b0b39677e6ce531dd8573ee2993829ec5dd0a
zhuxiuwu <1453855454@qq.com>
2018-12-26 18:09:40 +0800

    弹出菜单

--- src/screen/message/MessageScreen.js
+++ src/screen/message/MessageScreen.js
@@ -16,7 +16,7 @@ export default class MessageScreen extends Component {
     render() {
         const {navigation} = this.props;
         return (
-            <View>
+            <View style={{position:'relative'}}>
                 <StatusBar backgroundColor="#0187FB" barStyle="light-content" />
                 <View style={styles.header}>
                     <View style={styles.flex1}>
@@ -28,33 +28,49 @@ export default class MessageScreen extends Component {
                         <Text style={styles.title}>消息</Text>
                     </View>
                     <View style={styles.flex1}>
-                        <TouchableOpacity onPress={() => this.refs.son.setModalVisible(this.state.state)} style={{alignSelf:'flex-end',marginRight:10}}>
+                        <TouchableOpacity onPress={() => this.setState({ state: !this.state.state })} style={{alignSelf:'flex-end',marginRight:10}}>
                             <Text style={styles.add}>+</Text>
                         </TouchableOpacity>
                     </View>
                 </View>
-               <View>
-                   <MessageModal ref="son"/>
-               </View>
+                {
+                    this.state.state == false ?( null):(
+                        <View style={{width:100,height:260,backgroundColor:'#fff',position:'absolute',right:20,top:44,elevation: 99}}>
+                            <View
+                                style={{
+                                    width: 0,
+                                    height: 0,
+                                    borderColor: 'transparent',
+                                    borderTopWidth: 7,
+                                    borderBottomWidth: 7,
+                                    borderRightWidth: 10,
+                                    borderRightColor: '#79bd9a'
+                                }}
+                            />
+                            <View>
+                                <Text>//部位空时想要显示的内容</Text>
+                                <Text>//部位空时想要显示的内容</Text>
+                            </View>
+                        </View>
+                    )
+                }
                 <View>
-                    {
-                        this.state.state == false ?( null):(
-                            <Text>//部位空时想要显示的内容</Text>
-                        )
-                    }
+                    <View>
+                        <MessageModal ref="son"/>
+                    </View>
+                    <Button
+                        title='控制显示与隐藏'
+                        onPress={()=>{
+                            this.refs.son.setModalVisible(this.state.state)
+                        }}
+                    />
+                    <Button
+                        title='消息'
+                        onPress={()=>{
+                            navigation.goBack()
+                        }}
+                    />
                 </View>
-                <Button
-                    title='控制显示与隐藏'
-                    onPress={()=>{
-                        this.setState({ state: !this.state.state });
-                    }}
-                />
-                <Button
-                    title='消息'
-                    onPress={()=>{
-                        navigation.goBack()
-                    }}
-                />
             </View>
         );
     }
